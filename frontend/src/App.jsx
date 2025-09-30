4cb88fe25659284ae72bb0c3df29ab3722dd35cb
nvjib <nvjibiman@gmail.com>
2025-10-01 00:12:42 +0100

    feat(auth): add signup and login routes using React Router DOM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Signup/>
-      <Login/>
-    </div>
+      <Routes>
+          <Route path='/sign-up' element={<Signup/>}/>
+          <Route path='/login' element={<Login/>}/>
+      </Routes>
   )
 }
 
